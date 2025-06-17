b714c82236f720e738b024ef836ce83b19c52c02
cxrvus <benito.m.rabe@gmail.com>
2025-06-17 23:59:27 +0200

    instantiate empty map

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
+use crate::{map::Map, vec::Vec2};
+
 pub mod map;
 pub mod vec;
 
 fn main() {
-	println!("Hello, world!");
+	let mut board = Map::<u8>::new(8, 8);
+
+	board.set_at(&Vec2 { x: 4, y: 6 }, 1);
+
+	let board_str = board
+		.values
+		.iter()
+		.map(|x| match x {
+			0 => "..".to_string(),
+			_ => "##".to_string(),
+		})
+		.collect::<Vec<_>>()
+		.chunks(board.width)
+		.map(|chunk| chunk.join("") + "\n")
+		.collect::<String>();
+
+	println!("\n{board_str}\n");
 }
