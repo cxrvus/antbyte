a1bd2f4dd4d6564c9f708a46d1e2363c45eda87f
cxrvus <benito.m.rabe@gmail.com>
2025-07-07 23:48:12 +0200

    add get_row and get_col to Matrix

--- src/matrix.rs
+++ src/matrix.rs
@@ -56,6 +56,28 @@ where
 			y: self.height,
 		}
 	}
+
+	pub fn get_row(&self, i: usize) -> Option<Vec<&T>> {
+		if i >= self.height {
+			None
+		} else {
+			let start = i * self.width;
+			let end = (i + 1) * self.width;
+			let row = self.values[start..end].iter().collect::<Vec<&T>>();
+			Some(row)
+		}
+	}
+
+	pub fn get_col(&self, i: usize) -> Option<Vec<&T>> {
+		if i >= self.width {
+			None
+		} else {
+			let col = (0..self.height)
+				.map(|row| &self.values[row * self.width + i])
+				.collect::<Vec<&T>>();
+			Some(col)
+		}
+	}
 }
 
 impl<T> Matrix<T>
@@ -69,6 +91,51 @@ where
 			.filter(|(_, value)| **value == target)
 			.map(|(i, _)| self.get_pos(i).unwrap())
 			.collect()
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	fn create_test_matrix() -> Matrix<i32> {
+		Matrix {
+			width: 3,
+			height: 3,
+			values: vec![1, 2, 3, 4, 5, 6, 7, 8, 9],
+		}
+	}
+
+	#[test]
+	fn test_get_row() {
+		let m = create_test_matrix();
+
+		let row0 = m.get_row(0).unwrap();
+		assert_eq!(row0, vec![&1, &2, &3]);
+
+		let row1 = m.get_row(1).unwrap();
+		assert_eq!(row1, vec![&4, &5, &6]);
+
+		let row2 = m.get_row(2).unwrap();
+		assert_eq!(row2, vec![&7, &8, &9]);
+
+		assert!(m.get_row(3).is_none());
+	}
+
+	#[test]
+	fn test_get_col() {
+		let m = create_test_matrix();
+
+		let col0 = m.get_col(0).unwrap();
+		assert_eq!(col0, vec![&1, &4, &7]);
+
+		let col1 = m.get_col(1).unwrap();
+		assert_eq!(col1, vec![&2, &5, &8]);
+
+		let col2 = m.get_col(2).unwrap();
+		assert_eq!(col2, vec![&3, &6, &9]);
+
+		assert!(m.get_col(3).is_none());
 	}
 }
 
