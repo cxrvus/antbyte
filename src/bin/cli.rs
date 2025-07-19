08de8f5c6f7d9396747a864ea2e091d6d0ef2529
cxrvus <benito.m.rabe@gmail.com>
2025-07-19 02:06:41 +0200

    implement CLI echo

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,3 +1,20 @@
+use std::io::{self, IsTerminal, Read};
+
 fn main() {
+	if io::stdin().is_terminal() {
+		eprintln!("<!> no pipe input detected. please pipe data into this command.");
+		std::process::exit(1);
+	}
+
 	println!("<<ANTBYTE>>\n");
+
+	let mut buffer = String::new();
+
+	match io::stdin().read_to_string(&mut buffer) {
+		Ok(_) => println!("{buffer}"),
+		Err(e) => {
+			eprintln!("error reading from stdin: {e}");
+			std::process::exit(1);
+		}
+	}
 }
