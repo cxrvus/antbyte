8771bf34e873947d0b27efde7fb3d55192f8ee56
cxrvus <benito.m.rabe@gmail.com>
2025-07-22 02:28:10 +0200

    strip whitespace from tokens

--- src/ant/parser.rs
+++ src/ant/parser.rs
@@ -7,7 +7,7 @@ pub struct ParsedWorld;
 pub struct Parser;
 
 impl Parser {
-	const PATTERN: &'static str = r#"=>|[a-zA-Z]\w*|[#={}(),;1]|-"#;
+	const PATTERN: &'static str = r"=>|[a-zA-Z]\w*|[#={}(),;1]|-|\s+|.+";
 
 	pub fn parse(code: String) -> Result<ParsedWorld> {
 		let tokens = Self::tokenize(code);
@@ -17,19 +17,22 @@ impl Parser {
 	}
 
 	fn tokenize(code: String) -> Vec<Token> {
+		let whitespace_re = Regex::new(r"\s+").unwrap();
+
 		Regex::new(Self::PATTERN)
 			.unwrap()
 			.find_iter(&code)
-			.map(|x| Token::from(x.as_str()))
+			.map(|x| x.as_str())
+			.filter(|x| !whitespace_re.is_match(x))
+			.map(Token::from)
 			.collect::<Vec<_>>()
 	}
 }
 
 #[derive(Debug)]
 pub enum Token {
-	Invalid,
+	Invalid(String),
 	Ident(String),
-	WhiteSpace,
 }
 
 impl From<&str> for Token {
