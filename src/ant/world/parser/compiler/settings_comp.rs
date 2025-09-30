a2b46dc0a4f4cd7ab70732f32b42c1a1c6adce40
cxrvus <benito.m.rabe@gmail.com>
2025-09-30 02:01:13 +0200

    major clean up settings_comp

--- src/ant/world/parser/compiler/settings_comp.rs
+++ src/ant/world/parser/compiler/settings_comp.rs
@@ -1,137 +1,95 @@
-use anyhow::{Result, anyhow, bail};
+use anyhow::{Context, Result, anyhow};
 
 use crate::ant::{
 	BorderMode, ColorMode, StartingPos,
-	world::{WorldConfig, parser::token::Token},
+	world::{
+		WorldConfig,
+		parser::{Parser, token::Token},
+	},
 };
 
 const FPS_CAP: u32 = 60;
+const SIZE_CAP: u32 = 0x100;
 const SPEED_CAP: u32 = 0x2000;
 
 impl WorldConfig {
 	pub fn set_setting(&mut self, key: String, value: Token) -> Result<()> {
-		// todo: more elegant match block
-		match key.as_str() {
+		let mut parser = Parser {
+			tokens: vec![value],
+		};
+
+		parser
+			.set_setting(self, &key)
+			.with_context(|| format!("for setting '{key}'!"))
+	}
+}
+
+impl Parser {
+	fn set_setting(&mut self, config: &mut WorldConfig, key: &str) -> Result<()> {
+		match key {
 			key @ ("height" | "width" | "size") => {
-				if let Token::Number(value) = value {
-					expect_non_zero(value, key)?;
+				let value = self
+					.next_number(Some(SIZE_CAP))?
+					.ok_or_else(|| anyhow!("number must be greater than 0"))? as usize;
 
-					let value = value.clamp(1, 0x100) as usize;
-
-					match key {
-						"width" => self.width = value,
-						"height" => self.height = value,
-						"size" => {
-							self.width = value;
-							self.height = value;
-						}
-						_ => unreachable!(),
+				match key {
+					"width" => config.width = value,
+					"height" => config.height = value,
+					"size" => {
+						config.width = value;
+						config.height = value;
 					}
+					_ => unreachable!(),
+				}
+			}
 
-					Ok(())
-				} else {
-					invalid_type(&value, "number (pixel count)", key)
-				}
-			}
-			"fps" => {
-				if let Token::Number(value) = value {
-					self.fps = non_zero(value, Some(FPS_CAP));
+			"fps" => config.fps = self.next_number(Some(FPS_CAP))?,
+			"speed" => config.speed = self.next_number(Some(SPEED_CAP))?,
+			"ticks" => config.ticks = self.next_number(None)?,
+			"seed" => config.noise_seed = self.next_number(None)?,
 
-					Ok(())
-				} else {
-					invalid_type(&value, "number (FPS)", &key)
-				}
-			}
-			"speed" => {
-				if let Token::Number(value) = value {
-					self.speed = non_zero(value, Some(SPEED_CAP));
+			"loop" => config.looping = self.next_bit()?,
 
-					Ok(())
-				} else {
-					invalid_type(&value, "number (speed / ticks per frame)", &key)
-				}
-			}
-			"ticks" => {
-				if let Token::Number(value) = value {
-					self.ticks = non_zero(value, None);
+			"border" => config.border_mode = BorderMode::try_from(self.next_ident()?)?,
+			"start" => config.starting_pos = StartingPos::try_from(self.next_ident()?)?,
+			"colors" => config.color_mode = ColorMode::try_from(self.next_ident()?)?,
 
-					Ok(())
-				} else {
-					invalid_type(&value, "number (max ticks)", &key)
-				}
-			}
-			"loop" => {
-				match value {
-					Token::Bit(value) => self.looping = value,
-					value => return invalid_type(&value, "bit (is looping?)", &key),
-				}
-				Ok(())
-			}
-			"border" => {
-				if let Token::Ident(border_mode) = value {
-					self.border_mode = BorderMode::try_from(border_mode)?;
-					Ok(())
-				} else {
-					invalid_type(&value, "border mode (identifier)", &key)
-				}
-			}
-			"start" => {
-				if let Token::Ident(starting_pos) = value {
-					self.starting_pos = StartingPos::try_from(starting_pos)?;
-					Ok(())
-				} else {
-					invalid_type(&value, "starting pos (identifier)", &key)
-				}
-			}
-			"colors" => {
-				if let Token::Ident(color_mode) = value {
-					self.color_mode = ColorMode::try_from(color_mode)?;
-					Ok(())
-				} else {
-					invalid_type(&value, "color mode (identifier)", &key)
-				}
-			}
-			"seed" => {
-				if let Token::Number(seed) = value {
-					self.noise_seed = Some(seed);
-					Ok(())
-				} else {
-					invalid_type(&value, "seed value (number)", &key)
-				}
-			}
-			"desc" | "description" => {
-				if let Token::String(desc) = value {
-					self.description = desc;
-					Ok(())
-				} else {
-					invalid_type(&value, "description (string)", &key)
-				}
-			}
-			other => Err(anyhow!("unknown setting: '{other}'")),
+			"desc" | "description" => config.description = self.next_str()?,
+
+			other => return Err(anyhow!("unknown setting: '{other}'")),
+		}
+
+		Ok(())
+	}
+
+	fn next_number(&mut self, max: Option<u32>) -> Result<Option<u32>> {
+		let token = self.next_token();
+		if let Token::Number(value) = token {
+			// ensure number is non-zero
+			Ok(match value {
+				0 => None,
+				value => Some(match max {
+					None => value,
+					Some(max) => value.max(max),
+				}),
+			})
+		} else {
+			Err(Self::unexpected(token, "number"))
 		}
 	}
-}
 
-#[inline]
-fn non_zero(value: u32, max: Option<u32>) -> Option<u32> {
-	match value {
-		0 => None,
-		value => Some(match max {
-			None => value,
-			Some(max) => value.clamp(1, max),
-		}),
+	#[rustfmt::skip]
+	fn next_str(&mut self) -> Result<String> {
+		let token = self.next_token();
+		if let Token::String(value) = token { Ok(value) }
+		else { Err(Self::unexpected(token, "string")) }
 	}
-}
 
-#[inline]
-fn expect_non_zero(value: u32, key: &str) -> Result<()> {
-	match value > 0 {
-		true => Ok(()),
-		false => Err(anyhow!("number must be greater than 0 for setting '{key}'")),
+
+	#[rustfmt::skip]
+	fn next_bit(&mut self) -> Result<bool> {
+		let token = self.next_token();
+		if let Token::Bit(value) = token { Ok(value) }
+		else { Err(Self::unexpected(token, "bit")) }
 	}
-}
-
-#[inline]
-fn invalid_type(actual: &Token, expected: &str, key: &str) -> Result<()> {
-	bail!("expected {expected}, got {actual:?} for setting '{key}'");
 }
