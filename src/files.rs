5e8af3a1698515ed5bd95706d3a134ab8538638d
cxrvus <benito.m.rabe@gmail.com>
2026-03-03 23:49:56 +0100

    implement JSON compilation

--- src/files.rs
+++ src/files.rs
@@ -9,18 +9,23 @@ use crate::ant::{
 
 pub fn compile_world_file(path: &PathBuf, log_cfg: &LogConfig) -> Result<WorldProperties> {
 	let code = read_file(path)?;
-
 	let extension = path.extension().unwrap_or_default().to_string_lossy();
 
-	if extension != "ant" {
-		bail!("ant files need to have a '.ant' extension");
-	}
+	match extension.as_ref() {
+		"ant" => compile_world(&code, log_cfg, Some(path))
+			.with_context(|| format!("compiler error in file '{}'!", path.to_string_lossy())),
 
-	compile_world(&code, log_cfg, Some(path))
-		.with_context(|| format!("compiler error in file '{}'!", path.to_string_lossy()))
+		"json" => compile_json(&code),
+
+		_ => bail!("input files need to have a '.ant', '.json' or '.js' extension"),
+	}
 }
 
 pub fn read_file(path: &PathBuf) -> Result<String> {
 	fs::read_to_string(path)
 		.with_context(|| format!("error reading file '{}'!", path.to_string_lossy()))
+}
+
+pub fn compile_json(code: &str) -> Result<WorldProperties> {
+	serde_json::from_str::<WorldProperties>(code).context("invalid JSON world file")
 }
