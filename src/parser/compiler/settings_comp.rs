59783c198fee21ed08a92daf0b048db5f46270f3
cxrvus <benito.m.rabe@gmail.com>
2026-05-07 02:13:24 +0200

    misc fmt

--- src/parser/compiler/settings_comp.rs
+++ src/parser/compiler/settings_comp.rs
@@ -65,19 +65,12 @@ impl Parser {
 			}
 
 			"start_pos" | "start" => config.start_pos = StartingPos::try_from(self.next_ident()?)?,
-
 			"start_dir" => config.start_dir = self.next_number()?.unwrap_or_default() as u8,
-
 			"start_tick" => config.start_tick = self.next_number()?.unwrap_or_default(),
-
 			"ant_limit" => config.ant_limit = self.next_number()?,
-
 			"color_mode" | "colors" => config.color_mode = ColorMode::try_from(self.next_ident()?)?,
-
 			"desc" | "description" => config.description = self.next_str()?,
-
 			"hide_ants" => config.hide_ants = self.next_bit()?,
-
 			"ascii" => config.ascii = Some(self.next_str()?),
 			"keys" => config.keys = Some(self.next_str()?),
 
