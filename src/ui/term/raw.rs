f49d33334c2557097d29b3eb229f56799bff3092
cxrvus <benito.m.rabe@gmail.com>
2026-06-18 02:43:36 +0200

    add foreground to raw mode

--- src/ui/term/raw.rs
+++ src/ui/term/raw.rs
@@ -1,8 +1,32 @@
-use crate::{util::vec2::Position, world::World};
+use crate::{
+	util::vec2::Position,
+	world::{World, config::RenderMask},
+};
 
 pub fn run(world: World) {
 	let mut world = world;
 	while let Some(frame) = world.next_frame_auto() {
+		// ## FG
+		if let RenderMask::None = world.config().fg {
+			println!("--");
+		} else {
+			for y in 0..world.config().height {
+				for x in 0..world.config().width {
+					let fg_value = frame
+						.fg
+						.get(&Position { x, y })
+						.copied()
+						.unwrap_or_default();
+
+					print!("{:02x}", fg_value);
+				}
+				println!();
+			}
+		}
+
+		print!("\n\n");
+
+		// ## BG
 		for y in 0..world.config().height {
 			for x in 0..world.config().width {
 				let bg_value = frame
@@ -16,6 +40,7 @@ pub fn run(world: World) {
 			println!();
 		}
 
+		// ## Metadata
 		println!("\n{}\n", world.metadata_str());
 		print!("\n\n\n");
 	}
