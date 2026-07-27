938bffcedc7b278bd8a6c2288466e68d92e25cbd
cxrvus <benito.m.rabe@gmail.com>
2026-07-27 05:07:39 +0200

    fix critical serialization bug

--- src/ant/sub_pin.rs
+++ src/ant/sub_pin.rs
@@ -36,6 +36,9 @@ impl SubPin {
 		let mut ident = self.pin.short_ident().to_owned();
 
 		if self.pin.definition().size > 1 {
+			if self.pin.definition().size > 8 {
+				ident.push_str(&format!("{:x}", self.channel));
+			}
 			ident.push_str(&format!("{:x}", self.line));
 		}
 
